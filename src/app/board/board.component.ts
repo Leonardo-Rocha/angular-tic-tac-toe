65dfa93c631ebc19eb2cac52cbaf9e06bce7889b
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-10-15 23:27:42 -0400

    Add nebular theme Add board and square components Add simple tic tac toe logic

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: string[];
+  xIsNext: boolean;
+  winner: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(): void {
+    this.squares = Array(9).fill('');
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player(): string {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(index: number): void {
+    if (!this.squares[index]) {
+      this.squares.splice(index, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.calculateWinner();
+  }
+
+  calculateWinner(): void {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    lines.forEach(line => {
+      const [a, b, c] = line;
+
+      if (this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        this.winner = this.squares[a];
+      }
+    });
+  }
+}
